1deff4ef81552fbcd0bb5d33adfeabb19acb62d7
Abhishek <abhishrm9072@gmail.com>
2025-10-06 22:45:55 +0530

    pattern6

--- pattern1.2/pattern1.cpp
+++ pattern1.2/pattern1.cpp
@@ -13,9 +13,9 @@ Implement the pattern printing logic inside a function called print1().
 #include <iostream>
 using namespace std;
 void print1(){
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
-            cout<<"*";
+    for(int i=1; i<5; i++){
+        for(int j=1; j<5; j++){
+            cout<<"*"<<" ";
         }
         cout<<endl;
     }
---
+++ pattern1.2/pattern7.cpp
@@ -0,0 +1,19 @@
+/*
+a a a a
+b b b b
+c c c c
+d d d d 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    for(char ch='a'; ch<='d'; ch++) {
+        for(int j=1; j<=4; j++) {
+
+            cout<<ch<<" "; 
+        }
+        cout<<endl;
+    }
+    return 0;
+}
