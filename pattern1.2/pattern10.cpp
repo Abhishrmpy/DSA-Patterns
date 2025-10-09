e8e4d0cfdf2194eb7fb5cfdedab087dbd8664697
Abhishek <abhishrm9072@gmail.com>
2025-10-10 00:07:01 +0530

    pattern10

--- pattern1.2/pattern10.cpp
+++ pattern1.2/pattern10.cpp
@@ -1,19 +1,20 @@
 /*
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
 */
 
 #include <iostream>
 using namespace std;
 int main(){
-    for (int i=1; i<=5; i++){
-        for(int j=1; j<=5; j++) {
+    for(int i=0; i<=5; i++){
+        for(int j=5; j>=1; j--){
             cout<<j<<" ";
         }
         cout<<endl;
     }
     return 0;
 }
+    
