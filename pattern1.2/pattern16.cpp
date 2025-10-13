8e73486388d1104259e755dfe5e18d4a61e67162
Abhishek <abhishrm9072@gmail.com>
2025-10-13 23:47:41 +0530

    pattern16 sorry for the two day gap

---
+++ pattern1.2/pattern16.cpp
@@ -0,0 +1,20 @@
+/*
+Print this pattern using for loop 
+1
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=1; i<=5; i++){
+        for(int j=i; j>=1; j--){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
