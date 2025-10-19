b73b180141da43e0a95e315ac98eae8a93993795
Abhishek <abhishrm9072@gmail.com>
2025-10-19 08:51:37 +0530

    hard pattern

--- .gitignore/myname.cpp
+++ .gitignore/myname.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n = 7; // height of each letter (try changing this for size!)
+    int n = 7; // height of each letter
 
     for (int i = 0; i < n; i++) {
 
