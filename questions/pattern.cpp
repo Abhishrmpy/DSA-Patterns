a20e2166a54d680a266e780956d4fcd1d3500402
Abhishek <abhishrm9072@gmail.com>
2025-10-21 22:33:01 +0530

    pattern 32

---
+++ questions/pattern.cpp
@@ -0,0 +1,24 @@
+
+/*
+    1
+   1 2
+  1   3
+ 1     4
+1 2 3 4 5
+
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) cout << " ";
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i || i == n) cout << j << " ";
+            else cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
